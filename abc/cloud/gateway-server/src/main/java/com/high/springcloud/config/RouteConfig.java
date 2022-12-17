b1452d5094f247b315750f1bc14a91cd4d479976
high <2303006873@qq.com>
2022-12-17 14:41:31 +0800

    Spring Cloud Gateway断言和路由配置

---
+++ abc/cloud/gateway-server/src/main/java/com/high/springcloud/config/RouteConfig.java
@@ -0,0 +1,23 @@
+package com.high.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author high
+ * @version 1.0
+ * @since 1.0
+ */
+@Configuration
+public class RouteConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("guochuang-id", r -> r.path("/guochuang").uri("https://www.bilibili.com"))
+                .route("dance-id", r -> r.path("/v/dance").uri("https://www.bilibili.com"))
+                .route("kichiku-id", r -> r.path("/v/kichiku").uri("https://www.bilibili.com"))
+                .build();
+    }
+}
