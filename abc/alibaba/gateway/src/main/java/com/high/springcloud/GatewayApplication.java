53a957c3c29e1636187578d10fc60eb52ad757de
high <2303006873@qq.com>
2022-12-21 13:52:40 +0800

    Spring Cloud Alibaba Nacos整合Gateway

---
+++ abc/alibaba/gateway/src/main/java/com/high/springcloud/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.high.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author high
+ * @version 1.0
+ * @since 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
