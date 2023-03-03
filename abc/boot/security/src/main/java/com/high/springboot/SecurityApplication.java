6e2dfd95e383127227aadf6751f223c6b5964752
high <2303006873@qq.com>
2023-03-03 21:13:51 +0800

    Spring Security前置配置

---
+++ abc/boot/security/src/main/java/com/high/springboot/SecurityApplication.java
@@ -0,0 +1,18 @@
+package com.high.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * @author high
+ * @version 1.0
+ * @since 1.0
+ */
+@SpringBootApplication
+@EnableWebSecurity // 开启Security
+public class SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+}
